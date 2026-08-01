f379eea0b187414b14e793dc6a62129c7d281b41
08Playjohn <veroxana96@gmail.com>
2026-07-31 21:18:38 -0300

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -61,7 +61,7 @@ if (btnEnviar) {
         let mensaje = "¡Hola Play John! Quiero realizar el siguiente pedido:\n\n";
         carrito.forEach(item => { mensaje += `• ${item.nombre} x${item.cantidad} - $${(item.precio * item.cantidad).toLocaleString('es-AR')}\n`; });
         mensaje += `\n*Total estimado: ${totalCarrito.innerText}*`;
-        window.open(`https://wa.me{encodeURIComponent(mensaje)}`, '_blank');
+        window.open(`https://wa.link/b336q6"{encodeURIComponent(mensaje)}`, '_blank');
     });
 }
 
